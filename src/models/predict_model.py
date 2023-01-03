2b4c33777324479c4ac9cc20b0d186d3b121d27e
William C <chenut.william@gmail.com>
2023-01-03 11:46:35 +0100

    Update predict_model.py

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,6 +1,8 @@
 # load model
 import joblib
 import numpy as np
+import warnings
+
 
 def load_model():
     return joblib.load('../../models/knn.joblib')
@@ -9,5 +11,7 @@ def predict_model(model, data):
     return model.predict(data)[0]
 
 def predict_from_json(model, json_data):
+    warnings.filterwarnings('ignore')
     data = np.array(list(json_data.values())).reshape(1, -1)
     return predict_model(model, data)
+
