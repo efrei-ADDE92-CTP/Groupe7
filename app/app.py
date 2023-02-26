c619ac2fe4f98a4877b29b4d5515369ba1a36caa
William C <chenut.william@gmail.com>
2023-02-26 17:46:04 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -25,6 +25,8 @@ metrics = PrometheusMetrics(app)
 
 # predict endpoint
 @app.route('/predict', methods=['POST'])
+@metrics.summary('requests_by_status', 'Request latencies by status',
+                 labels={'status': lambda r: r.status_code})
 @metrics.histogram('requests_by_status_and_path', 'Request latencies by status and path',
                    labels={'status': lambda r: r.status_code, 'path': lambda: request.path})
 def predict():
