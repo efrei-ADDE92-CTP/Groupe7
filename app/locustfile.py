62b2ee5e511cc8dc5db4b99f7d51a0663398291f
William C <chenut.william@gmail.com>
2023-02-28 11:26:58 +0100

    Update locustfile.py

--- app/locustfile.py
+++ app/locustfile.py
@@ -1,11 +1,25 @@
+import random
 from locust import HttpUser,task,between 
 
 class AppUser(HttpUser):
     wait_time = between(2,5)
 
+    #generate random deciaml number
+    def on_start(self):
+        self.sepal_length = round(random.uniform(4.3,7.9),1)
+        self.sepal_width = round(random.uniform(2.0,4.4),1)
+        self.petal_length = round(random.uniform(1.0,6.9),1)
+        self.petal_width = round(random.uniform(0.1,2.5),1)
+
 	# Endpoint
     @task
     def predict(self):
+        # self.client.post('/predict',json={
+        #     "sepal_length": round(random.uniform(4.3,7.9),1),
+        #     "sepal_width": round(random.uniform(2.0,4.4),1),
+        #     "petal_length": round(random.uniform(1.0,6.9),1),
+        #     "petal_width": round(random.uniform(0.1,2.5),1)
+        # })
 
         self.client.post('/predict',json={
             "sepal_length": 5.1,
